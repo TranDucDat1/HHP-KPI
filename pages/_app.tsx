3cf442ca3fa53635f26bf9de720ad664afc81917
NMTFullstack <ngomanhtruong123@gmail.com>
2023-07-22 17:05:10 +0700

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import Layout from '../components/Layout';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Layout from "../components/Layout";
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <Layout>
             <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
             </Head>
             <Component {...pageProps} />
         </Layout>
