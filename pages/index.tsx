6c1cd9bb96a5588ba8e4f3278f978711776eb830
Duc Dat <datnho53@gmail.com>
2023-07-24 14:22:27 +0700

    fix conflict icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,17 +12,6 @@ export default function Home() {
           <link rel="shortcut icon" href="https://api-booking-app-aws-ec2.onrender.com/default.png" />
        </Head>
        <div>
-           <h1> Home Page </h1>
-            <Link href='/about'>
-                  Go to about
-            </Link>
-           <p>
-            To effectively use Next.js, it helps to be familiar with JavaScript, React, and related web development concepts. But JavaScript and React are vast topics. How do you know when you’re ready to learn Next.js?
-            Welcome to the Next.js Foundations course! This beginner-friendly, example-led course will guide you through the prerequisite knowledge for Next.js. You will build a simple project step-by-step; starting with a JavaScript application, then migrating it to React and Next.js.
-           </p>
-           <Link href='/coders'>
-                  Go to list coder
-            </Link>
        </div>
     </>
   )
